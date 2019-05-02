270514aa7d8ebefc147b7f8cb2d731ef205d84c0
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-02 19:53:59 +0200

    Resultat i array istedet for objekt

--- forvaltningsinstans.js
+++ forvaltningsinstans.js
@@ -1,297 +1,119 @@
 {
-  "VV-FM-FM-01": {
-    "tittel": {
-      "nb": "Fylkesmannen i Østfold"
-    },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-01",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+  "items": [
+    {
+      "kode": "VV-FM-FM-01",
+      "navn": {
+        "nor": "Fylkesmannen i Østfold"
       }
-    ]
-  },
-  "VV-FM-FM-02": {
-    "tittel": {
-      "nb": "Fylkesmannen i Akershus"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-02",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-02",
+      "navn": {
+        "nor": "Fylkesmannen i Akershus"
       }
-    ]
-  },
-  "VV-FM-FM-03": {
-    "tittel": {
-      "nb": "Fylkesmannen i Oslo"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-03",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-03",
+      "navn": {
+        "nor": "Fylkesmannen i Oslo"
       }
-    ]
-  },
-  "VV-FM-FM-04": {
-    "tittel": {
-      "nb": "Fylkesmannen i Hedmark"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-04",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-04",
+      "navn": {
+        "nor": "Fylkesmannen i Hedmark"
       }
-    ]
-  },
-  "VV-FM-FM-05": {
-    "tittel": {
-      "nb": "Fylkesmannen i Oppland"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-05",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-05",
+      "navn": {
+        "nor": "Fylkesmannen i Oppland"
       }
-    ]
-  },
-  "VV-FM-FM-06": {
-    "tittel": {
-      "nb": "Fylkesmannen i Buskerud"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-06",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-06",
+      "navn": {
+        "nor": "Fylkesmannen i Buskerud"
       }
-    ]
-  },
-  "VV-FM-FM-07": {
-    "tittel": {
-      "nb": "Fylkesmannen i Vestfold"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-07",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-07",
+      "navn": {
+        "nor": "Fylkesmannen i Vestfold"
       }
-    ]
-  },
-  "VV-FM-FM-08": {
-    "tittel": {
-      "nb": "Fylkesmannen i Telemark"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-08",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-08",
+      "navn": {
+        "nor": "Fylkesmannen i Telemark"
       }
-    ]
-  },
-  "VV-FM-FM-09": {
-    "tittel": {
-      "nb": "Fylkesmannen i Aust-Agder"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-09",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-09",
+      "navn": {
+        "nor": "Fylkesmannen i Aust-Agder"
       }
-    ]
-  },
-  "VV-FM-FM-10": {
-    "tittel": {
-      "nb": "Fylkesmannen i Vest-Agder"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-10",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-10",
+      "navn": {
+        "nor": "Fylkesmannen i Vest-Agder"
       }
-    ]
-  },
-  "VV-FM-FM-11": {
-    "tittel": {
-      "nb": "Fylkesmannen i Rogaland"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-11",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-11",
+      "navn": {
+        "nor": "Fylkesmannen i Rogaland"
       }
-    ]
-  },
-  "VV-FM-FM-12": {
-    "tittel": {
-      "nb": "Fylkesmannen i Hordaland"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-12",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-12",
+      "navn": {
+        "nor": "Fylkesmannen i Hordaland"
       }
-    ]
-  },
-  "VV-FM-FM-14": {
-    "tittel": {
-      "nb": "Fylkesmannen i Sogn og Fjordane"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-14",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-14",
+      "navn": {
+        "nor": "Fylkesmannen i Sogn og Fjordane"
       }
-    ]
-  },
-  "VV-FM-FM-15": {
-    "tittel": {
-      "nb": "Fylkesmannen i Møre og Romsdal"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-15",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-15",
+      "navn": {
+        "nor": "Fylkesmannen i Møre og Romsdal"
       }
-    ]
-  },
-  "VV-FM-FM-18": {
-    "tittel": {
-      "nb": "Fylkesmannen i Nordland"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-18",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-18",
+      "navn": {
+        "nor": "Fylkesmannen i Nordland"
       }
-    ]
-  },
-  "VV-FM-FM-19": {
-    "tittel": {
-      "nb": "Fylkesmannen i Troms"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-19",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-19",
+      "navn": {
+        "nor": "Fylkesmannen i Troms"
       }
-    ]
-  },
-  "VV-FM-FM-20": {
-    "tittel": {
-      "nb": "Fylkesmannen i Finnmark"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-20",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-20",
+      "navn": {
+        "nor": "Fylkesmannen i Finnmark"
       }
-    ]
-  },
-  "VV-FM-FM-50": {
-    "tittel": {
-      "nb": "Fylkesmannen i Trøndelag"
     },
-    "foreldre": [
-      "VV-FM-FM"
-    ],
-    "relasjon": [
-      {
-        "kode": "AO-50",
-        "kant": "forvalter",
-        "kantRetur": "verneområder som forvaltes av",
-        "erSubset": true
+    {
+      "kode": "VV-FM-FM-50",
+      "navn": {
+        "nor": "Fylkesmannen i Trøndelag"
       }
-    ]
-  },
+    }
+  ],
   "meta": {
     "tittel": "Forvaltningsinstans",
-    "produsertUtc": "2019-05-02T09:05:58.649Z",
+    "produsertUtc": "2019-05-02T17:50:38.956Z",
     "utgiver": "Artsdatabanken",
-    "url": "https://raw.githubusercontent.com/Artsdatabanken/naturvern-data/master/forvaltningsinstans.js",
+    "url": "https://raw.githubusercontent.com/Artsdatabanken/naturvern/master/forvaltningsinstans.js",
     "tool": {
       "name": "naturvern-lastejobb",
       "url": "https://github.com/Artsdatabanken/naturvern-lastejobb"
