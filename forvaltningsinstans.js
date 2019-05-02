0de9042eb91ac66ac89c2c679919e1a676e5fdd0
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-05-02 11:41:02 +0200

    Forvaltningsinstanser

---
+++ forvaltningsinstans.js
@@ -0,0 +1,301 @@
+{
+  "VV-FM-FM-01": {
+    "tittel": {
+      "nb": "Fylkesmannen i Østfold"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-01",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-02": {
+    "tittel": {
+      "nb": "Fylkesmannen i Akershus"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-02",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-03": {
+    "tittel": {
+      "nb": "Fylkesmannen i Oslo"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-03",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-04": {
+    "tittel": {
+      "nb": "Fylkesmannen i Hedmark"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-04",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-05": {
+    "tittel": {
+      "nb": "Fylkesmannen i Oppland"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-05",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-06": {
+    "tittel": {
+      "nb": "Fylkesmannen i Buskerud"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-06",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-07": {
+    "tittel": {
+      "nb": "Fylkesmannen i Vestfold"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-07",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-08": {
+    "tittel": {
+      "nb": "Fylkesmannen i Telemark"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-08",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-09": {
+    "tittel": {
+      "nb": "Fylkesmannen i Aust-Agder"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-09",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-10": {
+    "tittel": {
+      "nb": "Fylkesmannen i Vest-Agder"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-10",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-11": {
+    "tittel": {
+      "nb": "Fylkesmannen i Rogaland"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-11",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-12": {
+    "tittel": {
+      "nb": "Fylkesmannen i Hordaland"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-12",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-14": {
+    "tittel": {
+      "nb": "Fylkesmannen i Sogn og Fjordane"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-14",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-15": {
+    "tittel": {
+      "nb": "Fylkesmannen i Møre og Romsdal"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-15",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-18": {
+    "tittel": {
+      "nb": "Fylkesmannen i Nordland"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-18",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-19": {
+    "tittel": {
+      "nb": "Fylkesmannen i Troms"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-19",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-20": {
+    "tittel": {
+      "nb": "Fylkesmannen i Finnmark"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-20",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "VV-FM-FM-50": {
+    "tittel": {
+      "nb": "Fylkesmannen i Trøndelag"
+    },
+    "foreldre": [
+      "VV-FM-FM"
+    ],
+    "relasjon": [
+      {
+        "kode": "AO-50",
+        "kant": "forvalter",
+        "kantRetur": "verneområder som forvaltes av",
+        "erSubset": true
+      }
+    ]
+  },
+  "meta": {
+    "tittel": "Forvaltningsinstans",
+    "produsertUtc": "2019-05-02T09:05:58.649Z",
+    "utgiver": "Artsdatabanken",
+    "url": "https://raw.githubusercontent.com/Artsdatabanken/naturvern-data/master/forvaltningsinstans.js",
+    "tool": {
+      "name": "naturvern-lastejobb",
+      "url": "https://github.com/Artsdatabanken/naturvern-lastejobb"
+    },
+    "elementer": 18
+  }
+}
---
+++ forvaltningsinstans.schema.js
@@ -0,0 +1,739 @@
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "VV-FM-FM-01": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-02": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-03": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-04": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-05": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-06": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-07": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-08": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-09": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-10": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-11": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-12": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-14": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-15": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-18": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-19": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-20": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "VV-FM-FM-50": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "object",
+          "properties": {
+            "nb": {
+              "type": "string"
+            }
+          }
+        },
+        "foreldre": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        },
+        "relasjon": {
+          "type": "array",
+          "items": {
+            "type": "object",
+            "properties": {
+              "kode": {
+                "type": "string"
+              },
+              "kant": {
+                "type": "string"
+              },
+              "kantRetur": {
+                "type": "string"
+              },
+              "erSubset": {
+                "type": "boolean"
+              }
+            }
+          }
+        }
+      }
+    },
+    "meta": {
+      "type": "object",
+      "properties": {
+        "tittel": {
+          "type": "string"
+        },
+        "produsertUtc": {
+          "type": "string"
+        },
+        "utgiver": {
+          "type": "string"
+        },
+        "url": {
+          "type": "string"
+        },
+        "tool": {
+          "type": "object",
+          "properties": {
+            "name": {
+              "type": "string"
+            },
+            "url": {
+              "type": "string"
+            }
+          }
+        },
+        "elementer": {
+          "type": "number"
+        }
+      }
+    }
+  }
+}
